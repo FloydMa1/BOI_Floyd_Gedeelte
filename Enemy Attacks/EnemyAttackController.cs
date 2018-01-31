9a7dbc282b49843fce8145d2eb0c928084bb9a15
FloydMa1 <22355@ma-web.nl>
2018-01-31 09:22:47 +0100

    All Scripts

---
+++ Enemy Attacks/EnemyAttackController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackController : MonoBehaviour {
+
+    public GameObject Head;
+    public GameObject Middle;
+    public GameObject Player;
+
+    Vector2 enemyVector;
+    Vector2 playerVector;
+
+    float angle;
+
+    EnemyShootAttack shoot;
+    EnemyBrimstoneAttack brimstone;
+    EnemyChargeAttack charge;
+    EnemyPopUpAttack popUp;
+
+	void Start () {
+        brimstone = Head.GetComponent<EnemyBrimstoneAttack>();
+        charge = Head.GetComponent<EnemyChargeAttack>();
+        popUp = Head.GetComponent<EnemyPopUpAttack>();
+        shoot = Head.GetComponent<EnemyShootAttack>();
+
+        enemyVector = Vector2.up;
+        angle = 0.0f;
+	}
+	
+
+	void Update () {
+        enemyVector = Middle.transform.position;
+        playerVector = Player.transform.position;
+        angle = Vector2.Angle(enemyVector, playerVector);
+
+        StartCoroutine(Initiate());
+	}
+
+     void OnGUI()
+    {
+        GUI.Label(new Rect(25, 25, 200, 40), "Angle Between Objects" + angle);
+    }
+    
+    IEnumerator Initiate()
+    {
+        yield return new WaitForSeconds(3);
+        StartCoroutine(checkAttack());
+    }
+
+    IEnumerator checkAttack()
+    {
+        
+        if (angle < 15 && angle < 60 && popUp.canPopUp == true)
+        {
+            popUp.StartCoroutine(popUp.PopUpAttack());
+            charge.canCharge = false;
+        }
+        if (angle > 16 && angle < 110 && charge.canCharge == true)
+        {
+            charge.StartCoroutine(charge.ChargeAttack());
+            popUp.canPopUp = false;
+            yield return new WaitForSeconds(6);
+            popUp.canPopUp = true;
+        }
+        if (angle > 111 && angle > 161 && popUp.canPopUp == true && charge.canCharge == true && shoot.canShoot == true)
+        {
+            shoot.StartCoroutine(shoot.Shoot());
+            charge.canCharge = false;
+            popUp.canPopUp = false;
+            yield return new WaitForSeconds(2);
+            charge.canCharge = true;
+            popUp.canPopUp = true;
+        }
+        if (angle > 61 && angle < 160 && charge.canCharge == true)
+        {
+            charge.StartCoroutine(charge.ChargeAttack());
+            popUp.canPopUp = false;
+            yield return new WaitForSeconds(6);
+            popUp.canPopUp = true;
+        }
+        
+
+    }
+    
+}
---
+++ EnemyBulletMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletMovement : MonoBehaviour {
+
+    Rigidbody2D riegid;
+    public float speeed;
+    Animator anim;
+
+	void Start () {
+        riegid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        var a = transform.eulerAngles;
+        a.z += Random.Range(0, 360);
+        transform.eulerAngles = a;
+	}
+	
+
+	void Update () {
+        transform.position += transform.right * speeed * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject, 2);
+            anim.SetBool("Destroy", true);
+        }
+    }
+}
